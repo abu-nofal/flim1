5c5384efacbb2ea57b2b71e3c5bee8301167ae8e
AhmadNofal <>
2021-04-14 11:53:18 +0000

    add app.js and write at it

---
+++ app.js
@@ -0,0 +1,38 @@
+
+// console.log('welcome');
+// console.log(1+2);
+// console.log('bateg',1+2);
+// var theName ='moh5';
+// console.log(theName);
+// var mosa = 4;
+// console.log(mosa+=mosa);
+// if (8 ==='8'){
+//   console.log('ya hala');
+// } else {
+//   console.log('hello'+' '+'becouse');
+// }
+// var userName=prompt('put your name ')
+// console.log(userName)
+// var yourAge= prompt('enter your age')
+// if (yourAge>=18){
+//   document.write('welcome'+' '+userName)}
+
+alert('welcome and hallo to ahamd chanal');
+alert('here you will find a best movies ever');
+var userName =prompt('enter your name please');
+console.log(userName);
+var userAge =prompt('enter your age please ');
+console.log(userAge);
+if (userAge>=18){
+  document.write('<img src="https://img.freepik.com/free-vector/welcome-modern-lettering_136321-864.jpg?size=626&ext=jpg ">');
+  document.write('welcome aguin'+' '+userName+' '+'you can enjoy with our web ');
+  
+} else if (userAge<18) {
+  alert('becouse you are under age you read the contant in the end of the page ')
+  document.write('<img src="https://static8.depositphotos.com/1431107/919/i/950/depositphotos_9199988-stock-photo-oops-icon.jpg ">');
+  document.write('welcome '+' '+ userName +' '+'becouse your age '+userAge+' '+ 'under 18  we are so sorry we will send you to the other web its for under age' )
+
+}
+
+
+
